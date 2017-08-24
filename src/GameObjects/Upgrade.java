9a9974a5205255526d91c0633d556bb9bfb918f2
mamazu <maxo.pech@gmail.com>
2017-08-24 15:52:07 +0200

    Added UpdateSlot rendering

--- src/GameObjects/Upgrade.java
+++ src/GameObjects/Upgrade.java
@@ -3,34 +3,54 @@ package GameObjects;
 import java.awt.*;
 
 public class Upgrade implements GameObject {
-    public static final int UPGRADE_HEIGHT = 50;
-    Rectangle dimensions;
+    public static final int UPGRADE_HEIGHT = 60;
+    private final Font font = new Font(null, Font.PLAIN, 20);
+    Rectangle dimension;
 
     private int amount = 0;
     private double price = 0;
     private String name = "Test Product";
+
+    private int index;
 
     public Upgrade(){
         this(0);
     }
 
     public Upgrade(int index){
-        dimensions = new Rectangle(0, index*UPGRADE_HEIGHT, 200, UPGRADE_HEIGHT);
+        this(index, "");
+    }
+
+    public Upgrade(int index, String name){
+        this.index = index;
+        dimension = new Rectangle(0, index*(UPGRADE_HEIGHT + 5), 200, UPGRADE_HEIGHT);
+        this.name = name;
     }
 
     @Override
     public boolean click(Point p) {
-        return false;
+        if (p.x < dimension.x || p.x > dimension.x + dimension.width) {
+            return false;
+        }
+        boolean hit = p.y >= dimension.y && p.y <= dimension.y + dimension.height;
+        if (hit) {
+            price++;
+            System.out.println("Hit Upgrade: " + index);
+        }
+        return hit;
     }
 
     @Override
     public void draw(Graphics g) {
         g.setColor(Color.RED);
-        g.fillRect(dimensions.x, dimensions.y, dimensions.width, dimensions.height);
+        g.fillRect(dimension.x, dimension.y, dimension.width, dimension.height);
 
-        // Drawing the text
+        // Drawing the name
         g.setColor(Color.BLACK);
-        g.setFont(new Font(null, Font.PLAIN, 12));
-        g.drawString(name, 100, 100);
+        g.setFont(font);
+        g.drawString(name, 5 + dimension.x, dimension.y + font.getSize());
+
+        // Price
+        g.drawString(String.valueOf(price), 5 + dimension.x, dimension.y + font.getSize() * 2);
     }
 }
--- src/GameObjects/Upgrades.java
+++ src/GameObjects/Upgrades.java
@@ -3,22 +3,29 @@ package GameObjects;
 import java.awt.*;
 
 public class Upgrades implements GameObject{
-    private Upgrade[] upgradeArray = new Upgrade[1];
+    private Upgrade[] upgradeArray = new Upgrade[2];
     private int yOffset = 0;
 
     public Upgrades(){
-        upgradeArray[0] = new Upgrade();
+        upgradeArray[0] = new Upgrade(0, "Test U1");
+        upgradeArray[1] = new Upgrade(1, "Test U2");
+
     }
 
     public void setXOffset(int xOffset){
         for (Upgrade upgrade : upgradeArray){
-            upgrade.dimensions.x = xOffset;
-            upgrade.dimensions.width = xOffset;
+            upgrade.dimension.x = xOffset;
+            upgrade.dimension.width = xOffset;
         }
     }
 
     @Override
     public boolean click(Point p) {
+        for (Upgrade upgrade : upgradeArray){
+            if(upgrade.click(p)){
+                return true;
+            }
+        }
         return false;
     }
 
