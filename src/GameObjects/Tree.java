f9ee2234a14057c2d2e2e05bcf3b4fafe176f5cb
mamazu <maxo.pech@gmail.com>
2017-08-27 01:29:51 +0200

    Removed score from tree

--- src/GameObjects/Tree.java
+++ src/GameObjects/Tree.java
@@ -8,22 +8,21 @@ import java.io.IOException;
 import java.nio.Buffer;
 
 public class Tree implements GameObject {
-    Rectangle dimension = new Rectangle(0, 0, 100, 200);
-    Image image;
+    private Rectangle dimension = new Rectangle(0, 0, 100, 200);
+    private Image image;
 
-    long wood = 0L;
-
-    public Tree() {}
+    public Tree() {
+    }
 
     public Tree(Rectangle rect) {
         dimension = rect;
     }
 
-    public void loadImage(){
+    public void loadImage() {
         try {
             BufferedImage bi = ImageIO.read(new File("/home/mamazu/IdeaProjects/WoodChopper/resources/tree.png"));
             image = bi.getScaledInstance(dimension.width, dimension.height, Image.SCALE_SMOOTH);
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.print("Could not find file");
             return;
         }
@@ -34,16 +33,12 @@ public class Tree implements GameObject {
         if (p.x < dimension.x || p.x > dimension.x + dimension.width) {
             return false;
         }
-        boolean hit = p.y >= dimension.y && p.y <= dimension.y + dimension.height;
-        if (hit) {
-            wood++;
-        }
-        return hit;
+        return p.y >= dimension.y && p.y <= dimension.y + dimension.height;
     }
 
     @Override
     public void draw(Graphics g) {
-        g.setColor(new Color(173,216,230));
+        g.setColor(new Color(173, 216, 230));
         g.fillRect(dimension.x, dimension.y, dimension.width, dimension.height);
         if (image != null)
             g.drawImage(image, dimension.x, dimension.y, null);
